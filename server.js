de335a7935bc9b530c31245b8ddccf1b0f98461a
Warawich Sureepitak <warawich@gmail.com>
2019-10-14 09:48:01 +0700

    add env

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 // app configuration
 app.set('port', (process.env.PORT || 3000));
 // setup our express application
